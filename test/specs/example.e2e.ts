80d19a289fcd10a08ff2754d7572d6d6e3767cdc
datnpt <DatNPT@fpt.com>
2025-07-18 23:01:21 +0700

    update

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -5,6 +5,7 @@ import LoginPage from "../pageobjects/login.page";
 describe("TC01 - Login Page Render", () => {
   it("should display username, password fields and login button", async () => {
     const loginPage = new LoginPage();
+
     await loginPage.open();
 
     await expect(loginPage.inputUsername).toBeDisplayed();
@@ -16,8 +17,8 @@ describe("TC01 - Login Page Render", () => {
 describe("TC02 - Login with missing fields", () => {
   it("should show required validation messages", async () => {
     const loginPage = new LoginPage();
-    await loginPage.open();
 
+    await loginPage.open();
     await loginPage.btnLogin.click();
 
     const usernameValidation = await $('//span[text()="Required"]');
@@ -30,6 +31,7 @@ describe("TC03 - Login with invalid credentials", () => {
     const loginPage = new LoginPage();
     await loginPage.open();
 
+    // Login
     await loginPage.login("wronguser", "wrongpass");
 
     // const error = await $("div.oxd-alert-content-text");
@@ -45,6 +47,7 @@ describe("TC04 - Successful Login", () => {
   it("should login and redirect to dashboard", async () => {
     const loginPage = new LoginPage();
 
+    // Login
     await loginPage.open();
     await loginPage.login("Admin", "admin123");
 
@@ -68,6 +71,87 @@ describe("TC05 - Logout functionality", () => {
 
     await expect(browser).toHaveUrl(
       expect.stringContaining("/web/index.php/auth/login")
+    );
+  });
+});
+
+describe("TC06 - Verify displayed username after login", () => {
+  it("should display correct user name in profile menu", async () => {
+    const loginPage = new LoginPage();
+
+    // Login
+    await loginPage.open();
+    await loginPage.login("Admin", "admin123");
+
+    await $('img[alt="profile picture"]').click();
+    const name = await $("p.oxd-userdropdown-name");
+    await expect(name).toHaveText("Tosca Academy");
+  });
+});
+
+describe("TC07 - Search Employee in PIM", () => {
+  it("should find employee by name", async () => {
+    const loginPage = new LoginPage();
+
+    // Login
+    await loginPage.open();
+    await loginPage.login("Admin", "admin123");
+
+    // Go to PIM
+    const pimLink = await $('a[href="/web/index.php/pim/viewPimModule"]');
+    await pimLink.waitForDisplayed({ timeout: 10000 });
+    await pimLink.click();
+
+    // Wait for Employee Information header
+    const header = await $("h5=Employee Information");
+    await header.waitForDisplayed({ timeout: 10000 });
+
+    // Search input handling
+    const searchInput = await $('input[placeholder="Type for hints..."]');
+    await searchInput.scrollIntoView();
+    await browser.waitUntil(
+      async () => {
+        return (
+          (await searchInput.isDisplayed()) && (await searchInput.isEnabled())
+        );
+      },
+      {
+        timeout: 10000,
+        timeoutMsg: "Search input is not displayed or not enabled after 10s",
+      }
+    );
+    await searchInput.click(); // để đảm bảo focus
+    await searchInput.setValue("John");
+    await $('button[type="submit"]').click();
+
+    const result = await $$("div.oxd-table-cell")[2]; // index bắt đầu từ 0
+    const text = await result.getText();
+    expect(text).toContain("John");
+  });
+});
+
+describe("TC08 - Add new employee in PIM", () => {
+  it("should add a new employee", async () => {
+    const loginPage = new LoginPage();
+
+    // Login
+    await loginPage.open();
+    await loginPage.login("Admin", "admin123");
+
+    // Go to PIM and Add Employee
+    const ButtonAdd = await $(
+      'button.oxd-button.oxd-button--medium.oxd-button--secondary[type="button"]'
+    );
+
+    await $('a[href="/web/index.php/pim/viewPimModule"]').click();
+    ButtonAdd.click();
+
+    await $('input[name="firstName"]').setValue("Datttt");
+    await $('input[name="lastName"]').setValue("Nguyen");
+    await $('button[type="submit"]').click();
+
+    await expect(browser).toHaveUrl(
+      expect.stringContaining("/pim/viewPersonalDetails")
     );
   });
 });
